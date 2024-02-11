a87d945f6c5fa0c6371c168a997c1eb359f4bbab
unityPod <112590118+unityPod@users.noreply.github.com>
2024-02-10 20:25:53 -0800

    Update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,12 @@ module.exports = {
   content: ["./client/*.{html,js}"],
   theme: {
     colors: {
+      'cyan':{
+        600: 'rgb(8 145 178)'
+      },
       'indigo': {
-        400: '#6366f1'
+        400: '#6366f1',
+        600: '#4f46e5'
       }
     },
     extend: {},
